a4a56c2c3a9c5b4fb9e43720919562a6572a033e
Gunasekhar0027 <49283210+Gunasekhar0027@users.noreply.github.com>
2020-04-13 10:42:50 +0530

    Updated
    Updated

---
+++ app/src/main/java/com/android/guna/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.android.guna.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+   private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        requestWindowFeature ( Window.FEATURE_NO_TITLE );//for full screen
+        getWindow ().setFlags ( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView ( R.layout.activity_main );
+        mywebView =(WebView)findViewById ( R.id.webView );
+        WebSettings webSettings = mywebView.getSettings ();
+        webSettings.setJavaScriptEnabled ( true );
+        mywebView.loadUrl ( "your web page link here" );
+        mywebView.setWebViewClient ( new WebViewClient () );//prevents our url to open in another browser
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack ()){
+            mywebView.goBack ();
+        }else{
+            super.onBackPressed ();
+
+        }
+
+    }
+}
